d85d0cf1eecf68d9f50095e68e5c4cd5e79a16b1
Aryan Sethi <aryan.rdps@gmail.com>
2023-09-09 23:41:26 +0530

    tested and did getCurrentPlayer api

--- data-collection/getCurrentPlayers.py
+++ data-collection/getCurrentPlayers.py
@@ -3,36 +3,38 @@ import json
 import csv
 
 
-def fetch_users(app_id):
-    # response_API = requests.get(f'https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1?appid={app_id}')
+def fetch_current_players(app_id):
+    # parse int to str
+    app_id = str(app_id) if isinstance(app_id, int) else app_id
 
     try:
         response_API = requests.get(
-            f'https://store.steampowered.com/api/appdetails?appids={app_id}&cc=91')
+            f'https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1?appid={app_id}')
         if response_API.status_code == 200:
             data = response_API.text
             parse_json = json.loads(data)
+            return parse_json['response']['player_count']
+        return "null"
 
-            # return parse_json['response']['player_count']
-            return parse_json
     except Exception as e:
-        print(f"Exception at {e}")
+        print(f"Exception (FETCH_CURRENT_PLAYERS) at {app_id} , \n{e}")
         return "null"
 
 # 25s -> 50reqs
 
 
 def main():
-    with open("games.csv", "r", encoding="utf8") as f:
+    with open("data/games.csv", "r", encoding="utf8") as f:
         reader = csv.reader(f)
         next(reader)
         rec = next(reader)
-        for i in range(1):
+        for i in range(4):
             id = rec[0]
-            print(id, fetch_users(id))
+            print(id, fetch_current_players(id))
 
             rec = next(reader)
 
 
 if __name__ == "__main__":
     main()
+    print(fetch_current_players(730))
